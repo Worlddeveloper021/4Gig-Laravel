323f3b375f2cd517823d3ebef14d5237fd9011d7
Mostafa.kh <safakh1394@gmail.com>
2022-01-11 11:58:41 +0330

    add expiration for generated tokens (1 week life time)

---
+++ tests/Feature/Api/V1/ExpirationTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature\Api\V1;
+
+use Tests\TestCase;
+use Illuminate\Support\Facades\Notification;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ExpirationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function ensure_sanctum_expiration_works()
+    {
+        Notification::fake();
+
+        $expiration_duration = config('sanctum.expiration', 0);
+
+        $response = $this->json('post', '/api/v1/register', [
+            'email' => 'test@test.com',
+            'password' => 'secret',
+            'password_confirmation' => 'secret',
+        ]);
+
+        $response->assertSessionDoesntHaveErrors()
+                ->assertOk()
+                ->assertJsonStructure(['token']);
+
+        $this->travelTo(now()->addMinutes($expiration_duration + 1));
+
+        $this->json('get', 'api/user', [], ['Authorization' => 'Bearer '.$response->json('token')])
+             ->assertUnauthorized();
+    }
+}
