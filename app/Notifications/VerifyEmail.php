50c97ded7ca911d3dd1119e0dc3ffce061c9f01e
Mostafa.kh <safakh1394@gmail.com>
2022-01-15 00:46:08 +0330

    override defualt url form sending email on registeration

--- app/Notifications/VerifyEmail.php
+++ app/Notifications/VerifyEmail.php
@@ -6,5 +6,15 @@ use Illuminate\Auth\Notifications\VerifyEmail as BaseVerifyEmail;
 
 class VerifyEmail extends BaseVerifyEmail
 {
-    //
+    
+    /**
+     * Get the verification URL for the given notifiable.
+     *
+     * @param  mixed  $notifiable
+     * @return string
+     */
+    protected function verificationUrl($notifiable)
+    {
+        return 'ok';
+    }
 }
